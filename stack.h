95e4e2fa7fac88c54c61a3cc4d71567dde80b6ca
Luis (LT) Carbonell <lt.carbonell@gmail.com>
2016-11-02 15:37:54 -0400

    Create prototype for Stack Class using LInked List class

---
+++ stack.h
@@ -0,0 +1,21 @@
+#include "linkedlist.h"
+
+class Stack {
+  // Data members
+private:
+  LinkedList list;
+  int topIndex = 0;
+  // Methods
+public:
+  void push(int value); // insert new value to top of list
+  int pop(); // Remove and return current top value
+  int top(); // Return current top value WITHOUT removing it
+
+  int size(); // return size of Stack
+  bool isEmpty(); // Return true if the stack is empty
+  void print(); // Prints stuff out
+  // Constructor
+  Stack() {
+    list = LinkedList();
+  }
+}
